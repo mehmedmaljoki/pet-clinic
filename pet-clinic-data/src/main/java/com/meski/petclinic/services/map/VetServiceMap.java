0b598ec7c097d4da727cb3e943c65834824107e6
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2021-11-05 23:39:56 +0100

    Implement Map Based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/meski/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.meski.petclinic.services.map;
+
+import com.meski.petclinic.model.Vet;
+import com.meski.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
